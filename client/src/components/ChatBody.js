330c5b4d0c1a41c42f8836f5a6dd3ff3df0d878e
nkshn <vovanika23413@gmail.com>
2020-10-07 14:05:01 +0300

    add styling for send form and fix some styles

--- client/src/components/ChatBody.js
+++ client/src/components/ChatBody.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './ChatBody.css';
+import { BiSend, BiSmile } from 'react-icons/bi';
 
 function ChatBody() {
   return (
     <div className="chat-container">
       <div className="chat-container_header">
-        <img width="32px" height="32px" alt="this is alt" title="this is title" src="https://randomuser.me/api/portraits/thumb/women/2.jpg" />
+        <img width="36px" height="36px" alt="this is alt" title="this is title" src="https://randomuser.me/api/portraits/thumb/women/2.jpg" />
         <div className="chat-container_header-status-dot"></div>
         <div className="chat-container_header-text">
           <h3 className="chat-container_header-title">name surname</h3>
@@ -24,7 +25,13 @@ function ChatBody() {
           <li className="chat-container_body-message sender-me">1</li>
         </ul>
       </div>
-      <div className="chat-container_send"></div>
+      <div className="chat-container_send">
+        <textarea placeholder="Type a message..." />
+        <div className="chat-container_send-buttons">
+          <button><BiSmile color="#3e55d2" size={20} /></button>
+          <button><BiSend color="#3e55d2" size={20} /></button>
+        </div>
+      </div>
     </div>
   )
 }
